928f52ebcfc2062f45121e634548c7a9cc12bfdb
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-25 12:04:09 -0300

    feat: client

--- src/main/java/me/dio/products/model/Client.java
+++ src/main/java/me/dio/products/model/Client.java
@@ -1,5 +1,29 @@
 package me.dio.products.model;
 
-public class Client {
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
+public class Client {
+	  @Id
+	  @GeneratedValue(strategy = GenerationType.AUTO)
+	  private Long id;
+	  private String name;
+	  @Embedded
+	  private Adress adress;
 }
