c04cf119a2f997d519aef060dc92669b2f05f090
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-25 12:34:53 -0300

    feat: item repository

--- src/main/java/me/dio/products/repository/ItemRepository.java
+++ src/main/java/me/dio/products/repository/ItemRepository.java
@@ -1,5 +1,11 @@
 package me.dio.products.repository;
 
-public interface ItemRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import me.dio.products.model.Item;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
 
 }
