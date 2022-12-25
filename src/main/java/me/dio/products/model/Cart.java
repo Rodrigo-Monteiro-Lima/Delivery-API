1df3f88c8b0025bc611d072a383fdb5d43d49276
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-25 11:48:01 -0300

    feat: cart

--- src/main/java/me/dio/products/model/Cart.java
+++ src/main/java/me/dio/products/model/Cart.java
@@ -1,5 +1,34 @@
 package me.dio.products.model;
 
-public class Cart {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import me.dio.products.enumeration.PaymentMethod;
 
+import javax.persistence.*;
+import java.util.List;
+
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
+public class Cart {
+	 @Id
+	  @GeneratedValue(strategy = GenerationType.AUTO)
+	  private Long id;
+	  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+	  @JsonIgnore
+	  private Client client;
+	  @OneToMany(cascade = CascadeType.ALL)
+	  private List<Item> itens;
+	  private Double total;
+	  @Enumerated
+	  private PaymentMethod PaymentMethod;
+	  private boolean closed;
 }
