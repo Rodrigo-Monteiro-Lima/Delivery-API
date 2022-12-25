c14d00483d6ba93953f26df3f0847193e6212214
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-25 12:00:06 -0300

    feat: restaurant

--- src/main/java/me/dio/products/model/Restaurant.java
+++ src/main/java/me/dio/products/model/Restaurant.java
@@ -1,5 +1,35 @@
 package me.dio.products.model;
 
-public class Restaurant {
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
+public class Restaurant {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Product> menu;
+	@Embedded
+	private Adress adress;
 }
