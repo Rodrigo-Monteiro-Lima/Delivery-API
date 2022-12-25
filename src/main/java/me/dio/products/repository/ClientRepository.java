f3cedab18a8322c60473d2e0134734a86e8565b8
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-25 12:35:45 -0300

    feat: client repository

--- src/main/java/me/dio/products/repository/ClientRepository.java
+++ src/main/java/me/dio/products/repository/ClientRepository.java
@@ -1,5 +1,11 @@
 package me.dio.products.repository;
 
-public interface ClientRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import me.dio.products.model.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
 
 }
