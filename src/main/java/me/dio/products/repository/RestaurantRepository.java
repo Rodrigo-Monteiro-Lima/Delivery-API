07c4ed7c997ed3c2d674c02d93adbea143e189cf
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-25 12:33:07 -0300

    feat: restaurant repository

--- src/main/java/me/dio/products/repository/RestaurantRepository.java
+++ src/main/java/me/dio/products/repository/RestaurantRepository.java
@@ -1,5 +1,11 @@
 package me.dio.products.repository;
 
-public interface RestaurantRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import me.dio.products.model.Restaurant;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
 }
