2641ac951df21f210f067d88fb4f1de373bf68e3
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-25 12:32:00 -0300

    feat: cart repository

--- src/main/java/me/dio/products/repository/CartRepository.java
+++ src/main/java/me/dio/products/repository/CartRepository.java
@@ -1,5 +1,11 @@
 package me.dio.products.repository;
 
-public interface CartRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import me.dio.products.model.Cart;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
 
 }
