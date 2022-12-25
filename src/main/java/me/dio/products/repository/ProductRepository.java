18ad55448ac805137970a8e7fc71a24fe6932b69
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-25 12:34:11 -0300

    feat: product repository

--- src/main/java/me/dio/products/repository/ProductRepository.java
+++ src/main/java/me/dio/products/repository/ProductRepository.java
@@ -1,5 +1,11 @@
 package me.dio.products.repository;
 
-public interface ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import me.dio.products.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
 
 }
