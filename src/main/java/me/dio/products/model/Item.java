0048c914421948b94a9df58ad28bb0d1457d6715
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-25 12:01:38 -0300

    feat: item

--- src/main/java/me/dio/products/model/Item.java
+++ src/main/java/me/dio/products/model/Item.java
@@ -1,5 +1,34 @@
 package me.dio.products.model;
 
-public class Item {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
+public class Item {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@OneToOne
+	private Product product;
+	private int amount;
+	@ManyToOne
+	@JsonIgnore
+	private Cart cart;
 }
