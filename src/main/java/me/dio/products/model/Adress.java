f3cd03257ae63ab6e3a9913c9dfe9f68d5b4643e
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-25 12:02:55 -0300

    feat: adress

--- src/main/java/me/dio/products/model/Adress.java
+++ src/main/java/me/dio/products/model/Adress.java
@@ -1,5 +1,18 @@
 package me.dio.products.model;
 
-public class Adress {
+import javax.persistence.Embeddable;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Embeddable
+@NoArgsConstructor
+public class Adress {
+	private String cep;
+	private String complement;
 }
